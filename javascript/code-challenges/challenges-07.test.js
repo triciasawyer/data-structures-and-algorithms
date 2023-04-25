6bad1033af19053b5d4e14f990bca4c79d1e394c
Tricia Sawyer <tricia.sawyer03@gmail>
2023-04-25 13:42:31 -0500

    complete challenges 2 and 3

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -34,23 +34,28 @@ const sortStarWarsCharacters = (starWarsArr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named removeThree
-that takes an index and an array.
-The function should removes three items in the array starting with the value at the index.
+x -Write a function named removeThree
+x -that takes an index and an array.
+x -The function should removes three items in the array starting with the value at the index.
 ------------------------------------------------------------------------------------------------ */
 
 const removeThree = (idx, arr) => {
   // Solution code here...
+  arr.splice(idx, 3);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named joinArray that takes an array and joins all of the elements together in one string on a space.
+x -Write a function named joinArray
+x -that takes an array
+x -and joins all of the elements together in one string on a space.
 ------------------------------------------------------------------------------------------------ */
 
 const joinArray = (arr) => {
   // Solution code here...
+  return arr.join(' ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -256,13 +261,13 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array with three items removed', () => {
     expect(removeThree(2, [1, 2, 3, 4, 5, 6, 7, 8])).toStrictEqual([1, 2, 6, 7, 8]);
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should join an array', () => {
     expect(joinArray(['hello', '301', 'students'])).toStrictEqual('hello 301 students');
   });
