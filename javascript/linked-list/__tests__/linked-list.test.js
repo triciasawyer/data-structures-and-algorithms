d78f5a8e72c6531629db8e6e171070f6390a783f
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-21 11:03:07 -0500

    get all 7 tests passing!

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,37 +1,65 @@
 'use strict';
 
 
-const LinkedList = require('./index');
+const LinkedList = require('../index');
 
 
 describe('Linked List', () => {
   test('Can successfully instantiate an empty linked list', () => {
-    const list = new LinkedList();
+    let newList = new LinkedList();
 
-    expect(list.head).toBeNull();
+    expect(newList.head).toBeNull();
   });
+
+
   test('Can properly insert into the linked list', () => {
-    const list = new LinkedList();
-    list.insert(1);
+    let newList = new LinkedList();
+    newList.insert(1);
 
-    expect(list.head.value).toEqual(1);
-    expect(list.head.next).toBeNull();
+    expect(newList.head.value).toEqual(1);
+    expect(newList.head.next).toBeNull;
   });
+
+
   test('The head property will properly point to the first node in the linked list', () => {
-    const list = new LinkedList();
+    let list = new LinkedList();
     list.insert(2);
 
     expect(list.head.value).toEqual(2);
     expect(list.head.next).toBeNull();
+  })
 
-    // Can properly insert multiple nodes into the linked list
+    test('Can properly insert multiple nodes into the linked list', () => {
+    let list = new LinkedList();
     list.insert(1);
-    expect(list.head.value).toEqual(1);
-    expect(list.head.next.value).toEqual(2);
-    expect(list.head.next.next).toBeNull();
-    list.append('a');
-    expect(list.head.next.next.value).toEqual('a');
+    list.insert(2);
+    expect(list.head.value).toEqual(2);
+    expect(list.head.next.value).toEqual(1);
   });
 
 
+  test('Will return true when finding a value within the linked list that exists', () => {
+    let list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    expect(list.includes(2)).toBeTruthy();
+  });
+
+
+  test('Will return false when searching for a value in the linked list that does not exist', () => {
+    let list = new LinkedList();
+    list.insert(1);
+    list.insert(2);
+    expect(list.includes(3)).toBeFalsy();
+  });
+
+
+  test('Can properly return a collection of all the values that exist in the linked list', () => {
+    let list = new LinkedList();
+    list.insert('{ c }');
+    list.insert('{ b }');
+    list.insert('{ a }');
+    expect(list.toString()).toEqual('{ a } -> { b } -> { c } -> NULL');
+  });
 });
+
