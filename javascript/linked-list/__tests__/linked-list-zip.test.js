f93b8c06eb39a14b3d8eec33dc455870ac8f15de
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-24 13:54:57 -0500

    complete stretch goal I think

--- javascript/linked-list/__tests__/linked-list-zip.test.js
+++ javascript/linked-list/__tests__/linked-list-zip.test.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// Require our linked list implementation
+// Require our linked list implementations
 const LinkedList = require('../index');
-const { ZipLists } = require('../linked-list-zip.js');
+const { ZipLists, ZipSort } = require('../linked-list-zip.js');
 
 describe('Linked List Zip', () => {
-  test('Lists are equal length', () => {
+  test('Zip lists are equal length', () => {
     let list1 = new LinkedList();
     list1.append('{ 1 }');
     list1.append('{ 2 }');
@@ -17,7 +17,7 @@ describe('Linked List Zip', () => {
   });
 
 
-  test('List1 is longer than list2', () => {
+  test('Zip list1 is longer than list2', () => {
     let list1 = new LinkedList();
     list1.append('{ 1 }');
     list1.append('{ 2 }');
@@ -31,7 +31,7 @@ describe('Linked List Zip', () => {
   });
 
 
-  test('List2 is longer than list1', () => {
+  test('Zip list2 is longer than list1', () => {
     let list1 = new LinkedList();
     list1.append('{ 1 }');
     list1.append('{ 2 }');
@@ -45,13 +45,79 @@ describe('Linked List Zip', () => {
   });
 
 
-  test('List is empty', () => {
+  test('Zip list is empty', () => {
     let emptyList = new LinkedList;
     let list2 = new LinkedList();
     list2.append('{ 1 }');
     list2.append('{ 2 }');
 
     expect(ZipLists(emptyList, list2).toString()).toEqual('{ 1 } -> { 2 } -> NULL');
+  });
+
+
+  // stretch goal
+  test('ZipSort lists are equal length', () => {
+    let list1 = new LinkedList();
+    list1.append('{ a }');
+    list1.append('{ c }');
+    list1.append('{ d }');
+    let list2 = new LinkedList();
+    list2.append('{ b }');
+    list2.append('{ e }');
+    list2.append('{ f }');
+    expect(ZipSort(list1, list2).toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> { f } -> NULL');
+  });
+
+
+  test('ZipSort list1 is longer than list2', () => {
+    let list1 = new LinkedList();
+    list1.append('{ a }');
+    list1.append('{ d }');
+    list1.append('{ e }');
+    list1.append('{ f }');
+    let list2 = new LinkedList();
+    list2.append('{ b }');
+    list2.append('{ c }');
+    expect(ZipSort(list1, list2).toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> { f } -> NULL');
+  });
+
+
+  test('ZipSort list2 is longer than list1', () => {
+    let list1 = new LinkedList();
+    list1.append('{ b }');
+    list1.append('{ c }');
+    let list2 = new LinkedList();
+    list2.append('{ a }');
+    list2.append('{ d }');
+    list2.append('{ e }');
+    list2.append('{ f }');
+    expect(ZipSort(list1, list2).toString()).toEqual('{ a } -> { b } -> { c } -> { d } -> { e } -> { f } -> NULL');
+  });
+
+
+  test('ZipSort lists have equal values', () => {
+    let list1 = new LinkedList();
+    list1.append('{ a }');
+    list1.append('{ c }');
+    list1.append('{ d }');
+    let list2 = new LinkedList();
+    list2.append('{ c }');
+    list2.append('{ d }');
+    list2.append('{ e }');
+    expect(ZipSort(list1, list2).toString()).toEqual('{ a } -> { c } -> { c } -> { d } -> { d } -> { e } -> NULL');
+  });
+
+
+  test('ZipSort list is empty', () => {
+    let list1 = new LinkedList();
+    let list2 = new LinkedList();
+    list2.append(1);
+    list2.append(2);
+    list2.append(3);
+    list2.append(4);
+    list2.append(5);
+    list2.append(6);
+    expect(ZipSort(list1, list2).toString()).toEqual('1 -> 2 -> 3 -> 4 -> 5 -> 6 -> NULL');
   });
 
 
--- javascript/linked-list/linked-list-zip.js
+++ javascript/linked-list/linked-list-zip.js
@@ -1,4 +1,8 @@
+'use strict';
+
+
 const LinkedList = require('.');
+
 
 let ZipLists = (list1, list2) => {
   let current1 = list1.head;
@@ -23,5 +27,34 @@ let ZipLists = (list1, list2) => {
 };
 
 
+let ZipSort = (list1, list2) => {
+  let current1 = list1.head;
+  let current2 = list2.head;
+  let newList = new LinkedList();
+  if (current1?.value <= current2?.value || !current2) {
+    newList.head = current1;
+    current1 = current1.next;
+  } else if (current2?.value <= current1?.value || !current1){
+    newList.head = current2;
+    current2 = current2.next;
+  }
+  let currentResult = newList.head;
 
-module.exports = { ZipLists} ;
+  while (current1 || current2) {
+    while (current1 && (current1?.value <= current2?.value || !current2)) {
+      currentResult.next = current1;
+      currentResult = currentResult.next;
+      current1 = current1.next;
+    }
+    while (current2 && (current2?.value <= current1?.value || !current1)) {
+      currentResult.next = current2;
+      currentResult = currentResult.next;
+      current2 = current2.next;
+    }
+  }
+  return newList;
+};
+
+
+
+module.exports = { ZipLists, ZipSort } ;
