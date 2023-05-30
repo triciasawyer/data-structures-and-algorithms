ac88f0fd2d749b231ca8fe10ed4d3871692ff23b
Tricia Sawyer <tricia.sawyer03@gmail>
2023-05-30 17:59:29 -0500

    start writing tests

---
+++ javascript/401-code-challenges/stack-and-queue/__tests__/pseudo-queue.test.js
@@ -0,0 +1,25 @@
+const { PseudoQueue } = require('../index');
+
+
+describe('PseudoQueue', () => {
+  test('Can successfully enqueue into a queue with two stacks', () => {
+    let queue = new PseudoQueue();
+    queue.enqueue(1);
+    queue.enqueue(1);
+    queue.enqueue(1);
+  });
+
+  test('Can successfully enqueue into a queue with two stacks', () => {
+
+  });
+
+  test('Can successfully enqueue into a queue with two stacks', () => {
+
+  });
+
+  test('Can successfully enqueue into a queue with two stacks', () => {
+
+  });
+
+
+});
--- javascript/401-code-challenges/stack-and-queue/index.js
+++ javascript/401-code-challenges/stack-and-queue/index.js
@@ -89,4 +89,32 @@ class Queue {
 }
 
 
-module.exports = { Stack, Queue };
+class PseudoQueue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  enqueue(value) {
+    while (!this.stack1.isEmpty()) {
+      this.stack2.push(this.stack1.pop());
+    }
+    this.stack1.push(value);
+    while (!this.stack2.isEmpty()) {
+      this.stack1.push(this.stack2.pop());
+    }
+  }
+
+  dequeue() {
+    if(this.stack1.isEmpty()) {
+      return null;
+    }
+    return this.stack1.pop();
+  }
+
+
+}
+
+
+
+module.exports = { Stack, Queue, PseudoQueue };
