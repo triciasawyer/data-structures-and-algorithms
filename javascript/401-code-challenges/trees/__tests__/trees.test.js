bf8c992ab208a24a69a995eac64f6dc7f1e2a682
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-04 20:51:46 -0500

    add more tests

--- javascript/401-code-challenges/trees/__tests__/trees.test.js
+++ javascript/401-code-challenges/trees/__tests__/trees.test.js
@@ -11,19 +11,34 @@ describe('Trees', () => {
   });
 
 
-  // test('Can successfully instantiate a tree with a single root node', () => {
+  test('Can successfully instantiate a tree with a single root node', () => {
+    const treeWithRoot = new BinarySearchTree();
+    treeWithRoot.add(5);
 
-  // });
-
-
-  // test('For a Binary Search Tree, can successfully add a left child and right child properly to a node', () => {
-
-  // });
+    expect(treeWithRoot.root.value).toEqual(5);
+  });
 
 
-  // test('Can successfully return a collection from a pre-order traversal', () => {
+  test('For a Binary Search Tree, can successfully add a left child and right child properly to a node', () => {
+    const bst = new BinarySearchTree();
+    bst.add(5);
+    bst.add(3);
+    bst.add(7);
 
-  // });
+    expect(bst.root.value).toEqual(5);
+    expect(bst.root.left.value).toEqual(3);
+    expect(bst.root.right.value).toEqual(7);
+  });
+
+
+  test('Can successfully return a collection from a pre-order traversal', () => {
+    const bst = new BinarySearchTree();
+    bst.add(5);
+    bst.add(3);
+    bst.add(7);
+
+    expect(bst.preOrder()).toEqual([5, 3, 7]);
+  });
 
 
   // test('Can successfully return a collection from an in-order traversal', () => {
