4bb71a53bd20424ae7c0f34477e23dbf653c535d
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-05 20:11:44 -0500

    build tests and get code working

--- javascript/401-code-challenges/trees/__tests__/kary-trees.test.js
+++ javascript/401-code-challenges/trees/__tests__/kary-trees.test.js
@@ -1,34 +1,63 @@
 'use strict';
 
-const { KaryTree } = require('../index');
+
+const { KaryTree } = require('../index-K');
 
 
 describe('KaryTrees', () => {
-  test('Can successfully add a single node to an empty K-ary tree', () => {
-    const kTree = new KaryTree(3);
-    kTree.add(5);
+  let karyTree;
 
-    expect(kTree.root.value).toEqual(5);
+  beforeEach(() => {
+    karyTree = new KaryTree(3);
   });
 
 
-  test('Can successfully add multiple nodes to an empty K-ary tree', () => {
-    const kTree = new KaryTree(2);
-    kTree.add(5);
-    kTree.add(3);
-    kTree.add(8);
-    kTree.add(2);
-    kTree.add(7);
-
-
-    expect(kTree.root.value).toEqual(5);
-    expect(kTree.root.children[0].value).toEqual(3);
-    expect(kTree.root.children[1].value).toEqual(8);
-    expect(kTree.root.children[0].children[0].value).toEqual(2);
-    expect(kTree.root.children[1].children[0].value).toEqual(7);
-// kTree.root.children[1].children[0].value should be 7
+  test('Can instantiate an emtpy k-ary tree', () => {
+    expect(karyTree.root).toBe(null);
   });
 
+
+  test('Can successfully add nodes to an empty K-ary tree', () => {
+    karyTree.add(1);
+
+    expect(karyTree.root.value).toBe(1);
+    expect(karyTree.root.children.length).toBe(0);
+  });
+
+
+  test('Can add multiple nodes to a K-ary tree', () => {
+    karyTree.add(1);
+
+    expect(karyTree.root.value).toBe(1);
+    expect(karyTree.root.children.length).toBe(0);
+
+    karyTree.add(2, 1);
+
+    expect(karyTree.root.children.length).toBe(1);
+    expect(karyTree.root.children[0].value).toBe(2);
+
+    karyTree.add(3, 1);
+
+    expect(karyTree.root.children.length).toBe(2);
+    expect(karyTree.root.children[1].value).toBe(3);
+  });
+
+
+  test('Checks if a value exists in the K-ary tree', () => {
+    karyTree.add(1);
+    karyTree.add(2, 1);
+    karyTree.add(3, 1);
+    karyTree.add(4, 2);
+    karyTree.add(5, 2);
+
+    expect(karyTree.contains(1)).toBe(true);
+    expect(karyTree.contains(5)).toBe(true);
+    expect(karyTree.contains(6)).toBe(false);
+  });
 
 
 });
+
+
+module.exports = { KaryTree };
+
