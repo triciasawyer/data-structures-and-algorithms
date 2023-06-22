6480793f4bc8547d6946dc3c10ddf7fc49a60df5
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-22 18:27:04 -0500

    add movie js

---
+++ javascript/401-code-challenges/movie-sort/movie.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const Movies = [
+  {
+    title: "The Avengers",
+    year: 2012,
+    genres: ["Action", "Adventure", "Sci-Fi"],
+  },
+  {
+    title: "Legally Blonde",
+    year: 2001,
+    genres: ["Comedy", "Romance"],
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    genres: ["Action", "Adventure", "Sci-Fi"],
+  },
+  {
+    title: "13 Hours: The Secret Soldiers of Benghazi",
+    year: 2016,
+    genres: ["War", "Action"],
+  },
+  {
+    title: "Hustle",
+    year: 2022,
+    genres: ["Sporty", "Drama"],
+  },
+  {
+    title: "We're the Millers",
+    year: 2013,
+    genres: ["Comedy", "Crime"],
+  },
+  {
+    title: "Dumb and Dumber",
+    year: 1994,
+    genres: ["Comedy", "Adventure"],
+  },
+  {
+    title: "Grown Ups",
+    year: 2010,
+    genres: ["Comedy", "Buddy"],
+  },
+  {
+    title: "The Fast and the Furious",
+    year: 2001,
+    genres: ["Crime", "Adventure"],
+  },
+  {
+    title: "The Parent Trap",
+    year: 1998,
+    genres: ["Family", "Comedy"],
+  },
+];
+
+
+module.exports = { Movies };
