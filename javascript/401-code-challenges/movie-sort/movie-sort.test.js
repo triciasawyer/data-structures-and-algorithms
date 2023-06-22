63baac38de3d19d345287b546c16af722d2f3a3d
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-22 18:45:06 -0500

    get tests passing and code working properly

--- javascript/401-code-challenges/movie-sort/movie-sort.test.js
+++ javascript/401-code-challenges/movie-sort/movie-sort.test.js
@@ -4,14 +4,14 @@ const { sortByRecentYear, sortAlphabeticallyByTitle } = require('./index');
 const { Movies } = require('./movie');
 
 describe('Sorting Functions', () => {
-  test('Sort by Recent Year - should sort movies by most recent year first', () => {
+  test('Can sort movies by most recent year first', () => {
     const sortedMovies = sortByRecentYear(Movies);
 
     expect(sortedMovies[0].year).toBeGreaterThanOrEqual(sortedMovies[1].year);
   });
 
 
-  test('Sort Alphabetically by Title - should sort movies alphabetically ignoring leading words', () => {
+  test('Can sort movies alphabetically by title', () => {
     const sortedMovies = sortAlphabeticallyByTitle(Movies);
 
     expect(sortedMovies[0].title.localeCompare(sortedMovies[1].title)).toBeLessThanOrEqual(0);
