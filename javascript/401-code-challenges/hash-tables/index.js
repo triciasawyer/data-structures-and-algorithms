1614627962970b3d3e233a85941541197754261b
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-26 15:28:00 -0500

    add code

---
+++ javascript/401-code-challenges/hash-tables/index.js
@@ -0,0 +1,74 @@
+'use strict';
+
+
+class Hashtable {
+  constructor(size = 100) {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash << 5) - hash + key.charCodeAt(i);
+      hash = hash & hash; // Convert to 32-bit integer
+    }
+    return Math.abs(hash) % this.size;
+  }
+
+  set(key, value) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    for (let entry of this.table[index]) {
+      if (entry[0] === key) {
+        entry[1] = value;
+        return;
+      }
+    }
+    this.table[index].push([key, value]);
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      return undefined;
+    }
+    for (let entry of this.table[index]) {
+      if (entry[0] === key) {
+        return entry[1];
+      }
+    }
+    return undefined;
+  }
+
+  has(key) {
+    const index = this._hash(key);
+    if (!this.table[index]) {
+      return false;
+    }
+    for (let entry of this.table[index]) {
+      if (entry[0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  keys() {
+    const keys = [];
+    for (let bucket of this.table) {
+      if (bucket) {
+        for (let entry of bucket) {
+          keys.push(entry[0]);
+        }
+      }
+    }
+    return keys;
+  }
+
+  hash(key) {
+    return this._hash(key);
+  }
+}
