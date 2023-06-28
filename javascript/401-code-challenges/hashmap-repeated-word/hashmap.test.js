4535aa68f35b8e165f6f438e597d6d74ac0a3da9
Tricia Sawyer <tricia.sawyer03@gmail>
2023-06-28 15:34:07 -0500

    edit tests and their names

--- javascript/401-code-challenges/hashmap-repeated-word/hashmap.test.js
+++ javascript/401-code-challenges/hashmap-repeated-word/hashmap.test.js
@@ -4,7 +4,15 @@ const { repeatedWord } = require('./index');
 
 
 describe('Hashmap repeated word', () => {
-  test('Can sort movies by most recent year first', () => {
+  test('Return null when there are no repeated words in the input string', () => {
+    const input3 = "It was just like a fairytale...";
+    const output3 = repeatedWord(input3);
+
+    expect(output3).toBeNull();
+  });
+
+
+  test('Correctly identifies the first repeated word in the input string', () => {
     const input1 = "Once upon a time, there was a brave princess who...";
     const output1 = repeatedWord(input1);
 
@@ -12,20 +20,13 @@ describe('Hashmap repeated word', () => {
   });
 
 
-  test('Can sort movies by most recent year first', () => {
+  test('Correctly identifies the first repeated word in a longer input string, with multiple repeated words', () => {
     let input2 = "It was the best of times, it was the worst of times, it was the age of wisdom...";
     let output2 = repeatedWord(input2);
 
     expect(output2).toEqual('it');
   });
 
-
-  test('Can sort movies by most recent year first', () => {
-    let input3 = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
-    let output3 = repeatedWord(input3);
-
-    expect(output3).toEqual('summer');
-  });
 
 });
 
